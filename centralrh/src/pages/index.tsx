a2e188ce9a9aaadad39a96a1506af21e8f1ddb50
Rafa1a <110539932+Rafa1a@users.noreply.github.com>
2023-03-20 22:23:28 -0300

    up

--- centralrh/src/pages/index.tsx
+++ centralrh/src/pages/index.tsx
@@ -21,30 +21,32 @@ export default function Home() {
     : 'bg-body-tertiary';
     const navbarTextColorClass = Darkmode ? 'text-light' : 'text-dark';
     
+    const navbartoggler = Darkmode ? 'navbar-dark' : 'navbar-light'
     
 
   return (
     <>
       <header>
-      <nav className={`navbar navbar-expand-lg ${navbarBackgroundColorClass}`}>
+      <nav className={`navbar navbar-expand-lg  ${navbarBackgroundColorClass} ${navbartoggler}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" href="/">Controll</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className={`navbar-brand ${navbarTextColorClass}`}  href="/">Controll</Link>
+        <button className="navbar-toggler navbar-toggler-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+              <Link className={`nav-link active ${navbarTextColorClass}`} aria-current="page" href="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" href="/">Sobre</Link>
+              <Link className={`nav-link ${navbarTextColorClass}`} href="/">Sobre</Link>
             </li>
           </ul>
+          
         </div>
-        <button className= {`btn btn-sm ${Darkmode ? 'btn-light' : 'btn-dark'}`} onClick={handleDarkModeToggle}>{Darkmode ? 'Light Mode' : 'Dark Mode'}</button>
-
+        <button className= {`btn btn-sm ${Darkmode ? 'btn-light' : 'btn-dark'}`} onClick={handleDarkModeToggle}>{Darkmode ? 'Dark Mode' : 'Light Mode'}</button>
       </div>
+
       </nav>
         </header>
       <main>
